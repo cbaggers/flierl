a0396f6b46a868b014a824cf1a0691a084691ae9
cbaggers <chris.bagley@gmail.com>
2018-07-09 14:50:51 +0200

    noob ahoy. Fudged in monitoring of compile process&fix some clauses
    case had some issues but we also timed out rather than monitoring,
    which was lazy and dumb (as it stalled emacs).

--- flierl.erl
+++ flierl.erl
@@ -21,37 +21,65 @@ transform_for_emacs(CompileResult) ->
            
             error -> 
                 "(((0 \"unknown error\")) nil)";
-            {error, [{_File0, Errors}], [{_File1, Warnings}]} -> 
+            {error, [{_File0, Errors}]} -> 
+                io_lib:format("(~s nil)", [sexp([process_error_info(E) || E <- Errors])]);
+            {error, [{_File0, Errors}], []} -> 
+                io_lib:format("(~s nil)", [sexp([process_error_info(E) || E <- Errors])]);
+            {error, [{_File0, Errors}, {_File1, Warnings}]} -> 
                 io_lib:format("(~s ~s)", [sexp([process_error_info(E) || E <- Errors]),
                                           sexp([process_error_info(W) || W <- Warnings])]);
-            {error, [{_File0, Errors}], []} -> 
-                io_lib:format("(~s nil)", [sexp([process_error_info(E) || E <- Errors])])
+            {error, [{_File0, Errors}, {_File1, Warnings}], _} -> 
+                io_lib:format("(~s ~s)", [sexp([process_error_info(E) || E <- Errors]),
+                                          sexp([process_error_info(W) || W <- Warnings])])
         end,
     lists:flatten(Sexp).
 
 
 compile_and_report(Path, ReportTo) ->
+    Self = self(),
     Result = 
         try
             Report = compile:file(Path, [strong_validation, return]),
             transform_for_emacs(Report)
         catch
             Err ->
+                erlang:display("SHI"),
                 io_lib:format("(((0 \"Flierl Compile Error: ~p\")) nil)", 
                               [Err])
         end,
-    ReportTo ! {self(), Result}.
+    erlang:display({"CockFace! reporting to", ReportTo, {Self, Result}}),
+    ReportTo ! {Self, Result}.
+
+clear_mailbox() ->
+    receive
+        _Any ->
+            clear_mailbox()
+    after 0 ->
+        ok
+    end.
+
+await_compile_result() ->
+    receive
+        {_Pid, News} ->
+            News;
+        {'DOWN', _Ref, _Type, _Object, normal} ->
+            await_compile_result();
+        {'DOWN', _Ref, Type, Object, Info} ->
+            erlang:display({flierl_monitored_error, Type, Object, Info}),
+            "(((0 \"unknown flierl error\")) nil)";
+        Err -> 
+            erlang:display({flierl_error, Err}),
+            "(((0 \"unknown flierl error\")) nil)"
+    after
+        3000 ->
+            "(((0 \"compile timed out\")) nil)"
+    end.
+    
 
 compile(Path) ->
     Self = self(),
-    Pid = spawn(fun () -> compile_and_report(Path, Self) end),
-    Result = receive
-                 {Pid, News} ->
-                     News;
-                 _ -> 
-                     "(((0 \"unknown flierl error\")) nil)"
-             after
-                 3000 ->
-                     "(((0 \"compile timed out\")) nil)"
-             end,
+    {Pid, Ref} = spawn_monitor(fun () -> compile_and_report(Path, Self) end),
+    erlang:display({i, Self, spawned, Pid, with, Ref}),
+    Result = await_compile_result(),
+    clear_mailbox(),
     io:format("~s", [Result]).
